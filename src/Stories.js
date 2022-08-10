eed8dc53a66fe8d2b9ee64a009ca8fb0eb65d10d
Thomas Pendleton <thomas6576@yahoo.com>
2022-08-09 20:32:22 -0700

    fetch call in context

--- src/Stories.js
+++ src/Stories.js
@@ -3,7 +3,20 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const Stories = () => {
-  return <h2>stories component</h2>
+  const { isLoading, hits, nbPages } = useGlobalContext();
+
+  if (isLoading) {
+    return <div className="loading"> </div>;
+  }
+
+  return (
+    <section className="stories">
+      {hits.map((story) => {
+        const {objectID, title, author, url} = story
+        return <article key={objectID} className='story'>{story.title}</article>;
+      })}
+    </section>
+  );
 }
 
 export default Stories
--- src/context.js
+++ src/context.js
@@ -11,12 +11,46 @@ import reducer from './reducer'
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
 
-const initialState = {}
+const initialState = {
+  isLoading: true,
+  hits: [],
+  query: "react",
+  page: 0,
+  nbPages: 0,
+};
 
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const fetchNews = async (url) => {
+    dispatch({ type: SET_LOADING });
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data.hits);
+      dispatch({
+        type: SET_STORIES,
+        payload: { hits: data.hits, nbPages: data.nbPages },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    //add additional queries
+    fetchNews(`${API_ENDPOINT}query=${state.query}&page=${state.page}`);
+
+    return () => {
+      // second
+    };
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
 }
 // make sure use
 export const useGlobalContext = () => {
