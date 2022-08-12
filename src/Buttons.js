6d5e8af9adc8e10c309ca71af421b46a1efc1a0c
Thomas Pendleton <thomas6576@yahoo.com>
2022-08-11 21:43:54 -0700

    use global context in button

--- src/Buttons.js
+++ src/Buttons.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { useGlobalContext } from './context'
 
 const Buttons = () => {
+  const data = useGlobalContext();
+  console.log(data)
+  
   return <h2>button container</h2>
 }
 
